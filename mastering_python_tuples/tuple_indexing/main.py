d54811e91b16f384ef07389af0d84c87ab4a2a73
felipeschawsnekker <>
2026-07-05 23:22:12 +0000

    Tuple indexing

---
+++ mastering_python_tuples/tuple_indexing/main.py
@@ -0,0 +1,14 @@
+space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian')
+
+# Write your code here
+first_movie = space_movies[0];
+last_movie = space_movies[-1];
+
+favorite_movies = []
+favorite_movies.append(first_movie)
+favorite_movies.append(last_movie)
+
+# Testing 
+print("First movie: ",first_movie)
+print("Last movie: ",last_movie)
+print("Favorite movies about space:", favorite_movies)
